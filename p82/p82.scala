f56278fac12f8a0944e1e2deb8d46a6eaac17600
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-01-14 05:50:19 +0000

    Clean-Up Problem 82

--- p82/p82.scala
+++ p82/p82.scala
@@ -12,14 +12,6 @@ object p82 {
     .mkString.split("\n").toList
     .map(_.split(",").map(_.toInt).toList)
 
-  // val matrix = List(
-  //   List(131, 673, 234, 103, 18),
-  //   List(201, 96, 342, 965, 150),
-  //   List(630, 803, 746, 422, 111),
-  //   List(537, 699, 497, 121, 956),
-  //   List(805, 732, 524, 37, 331)
-  // )
-
   def columnSum(j: Int, state2: mutable.Map[Int, Seq[Int]]) = {
     state2.getOrElse(j, {
       val result = List.range(0,limit)
@@ -43,10 +35,10 @@ object p82 {
         val cs = columnSum(j, state2)
         val result = List.range(0,limit)
           .map({ i2 =>
+            val currCost = matrix(i)(j)
             val colCost = if (i <= i2) (cs(i2+1) - cs(i+1)); else (cs(i) - cs(i2))
-            val moo = matrix(i)(j) + colCost + optimize(i2, j+1, state1, state2)
-            // println(s"$i $j $moo ${optimize(i2, j+1, state1, state2)}")
-            moo
+            val tailCost = optimize(i2, j+1, state1, state2)
+            currCost + colCost + tailCost
           })
           .reduce(min(_,_))
 
@@ -60,11 +52,9 @@ object p82 {
   val state2 = mutable.Map.empty[Int, Seq[Int]]
 
   val solution = {
-    // List.range(limit,76,-1).foreach({ optimize(_,0,state) })
     List.range(0,limit)
       .map(optimize(_,0,state1,state2))
       .reduce(min(_,_))
-    // optimize(1,0,state1,state2)
   }
 
   def main(args: Array[String]) = println(solution)
