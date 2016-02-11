41455ef338235ba9388646553550433e44e7c711
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-02-11 01:55:03 +0000

    Problem 113 Solution

--- 110/p113/p113.scala
+++ 110/p113/p113.scala
@@ -4,40 +4,41 @@ import scala.collection.mutable
 
 object p113 {
 
-  def nonDecreasing(
+  def nonBouncyHalf(
     index: Int,
-    limit: Int,
+    n: Int,
     digit: Int,
-    state: mutable.Map[(Int, Int), Int] = mutable.Map.empty[(Int, Int), Int],
-    backup: String = ""
-  ): Int = {
-    val nextBackup = backup ++ digit.toString
-
-    if (index == limit) {
-      println(s"${nextBackup}")
-      1
-    }
+    nonDecreasing: Boolean,
+    state: mutable.Map[(Int, Int), BigInt] = mutable.Map.empty[(Int, Int), BigInt]
+  ): BigInt = {
+    if (index == n) BigInt(1)
     else {
       val key = (index, digit)
       val cached = state.get(key)
 
-      if (cached != None && false) cached.get
+      if (cached != None) cached.get
       else {
-        val result = (digit to 9).map({ d => nonDecreasing(index + 1, limit, d, state, nextBackup) }).sum
+        val range =
+          if (nonDecreasing) (digit to 9) // "Increasing"
+          else (0 to digit) // "Decreasing"
+        val result = range.map({ d => nonBouncyHalf(index + 1, n, d, nonDecreasing, state) }).sum
         state.put(key, result)
         result
       }
     }
   }
 
-  val solution = (0 to 9).map({ limit => nonDecreasing(0,2,limit) }).sum
+  def nonBouncy(zeros: Int): BigInt = {
+    val raw =
+      (0 until zeros).flatMap({ n =>
+        (1 to 9).map({ digit => nonBouncyHalf(0, n, digit, true) + nonBouncyHalf(0, n, digit, false) })
+      }).sum
+    val duplicate = 9 * zeros
 
-  def main(args: Array[String]) = {
-    println(solution)
-    println
-    println(Euler.natural
-      .takeWhile(_ < 1000)
-      .filter({ n => n.toString == n.toString.sorted /*&& n.toString != n.toString.sorted.reverse*/ })
-      .length)
+    raw - duplicate
   }
+
+  val solution = nonBouncy(100)
+
+  def main(args: Array[String]) = println(solution)
 }
