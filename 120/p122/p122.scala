c5446438edfbc5c80852b2b677ad9fd2783cbb9e
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-02-18 00:57:49 +0000

    Problem 122: Correct
    Use backtracking to search exhaustively.

--- 120/p122/p122.scala
+++ 120/p122/p122.scala
@@ -1,46 +1,40 @@
 package com.daystrom_data_concepts
 
 import scala.collection.mutable
-import math.{ceil, log}
+import math.{floor, log, min}
 
 
 object p122 {
+  val limit = 200
+  val defaultPath = Stream.iterate(1)(_ * 2).takeWhile(_ <= limit).toList
+  val defaultMap = mutable.Map(defaultPath.zip(Stream.iterate(0)(_ + 1)): _*)
+
   def m(
-    k: Int,
-    seen: Set[Int] = Set.empty[Int],
-    cache: mutable.Map[(Int, Set[Int]), Int] = mutable.Map.empty[(Int, Set[Int]), Int]
-  ): Int = {
-    if (k <= 1) 0
-    else if (seen.contains(k)) 0
-    // http://wwwhomes.uni-bielefeld.de/achim/addition_chain.html
-    else if (Set(31, 56, 44, 46, 41, 80, 42, 45, 60, 39, 54, 51, 96, 35, 52, 29, 50, 43, 68, 38, 37, 72, 49, 66, 65).contains(k)) 7
-    else if (Set(28, 22, 23, 40, 21, 30, 27, 48, 26, 25, 34, 19, 36, 33).contains(k)) 6
-    else if (Set(14, 11, 20, 15, 24, 13, 17, 18).contains(k)) 5
-    else if (Set(7,10,12,9).contains(k)) 4
-    else if (Set(5,6).contains(k)) 3
-    else {
-      val key = (k, seen)
-      val cached = cache.get(key)
-      val exp = log(k) / log(2)
+    path: List[Int] = defaultPath.reverse,
+    map: mutable.Map[Int, Int] = defaultMap,
+    fromStem: Int = 0
+  ): Unit = {
+    val n = path.head
 
-      if (exp == ceil(exp)) exp.toInt
-      else if (cached.nonEmpty) cached.get
-      else {
-        val result = (1 until k)
-          .flatMap({ x =>
-            val one = m(x,seen,cache) + m(k-x,seen+x,cache) + 1
-            val two = m(k-x,seen,cache) + m(x,seen+(k-x),cache) + 1
-            List(one, two) })
-          .reduce({ (l,r) => if (l < r) l; else r })
+    path.filter({ i => i + n <= limit })
+      .foreach({ i =>
+        val number = (i + n)
+        val newPath = number :: path
+        val current = map.getOrElse(number, Int.MaxValue)
+        val possible = newPath.length - 1
 
-        cache.put((k,seen), result)
-        result
-      }
-    }
+        if (possible <= current) {
+          map.put(number, possible)
+          m(path = newPath, map = map, fromStem = (fromStem + 1) )
+        }
+      })
+    if (fromStem == 0 &&  path.tail.nonEmpty) m(path.tail, map)
   }
 
-  val cache = mutable.Map.empty[(Int, Set[Int]), Int]
-  val solution = m(102, cache=cache)
+  val solution = {
+    m()
+    (1 to min(limit, 200)).map({ i => defaultMap.get(i).get }).sum
+  }
 
   def main(args: Array[String]) = println(solution)
 }
