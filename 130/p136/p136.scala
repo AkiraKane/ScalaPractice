0198ae1a6094ea146f2550cc8f81fb7979a4f91a
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-03-01 23:51:26 +0000

    Problem 136

---
+++ 130/p136/p136.scala
@@ -0,0 +1,30 @@
+package com.daystrom_data_concepts
+
+import scala.collection.mutable
+
+
+object p136 {
+  val limit = 50000000
+
+  val counts = new Array[Int](limit+1)
+
+  def compute() = {
+    var x = 1; while (x <= limit) {
+      var y = 1; while (x * y <= limit) {
+        val n = x * y
+        val d = (x + y) / 4
+        val a = y - d
+        if (3*d*d + 2*a*d - a*a == n && a > 0) counts(n) += 1
+        y += 1
+      }
+      x += 1
+    }
+  }
+
+  val solution = {
+    compute()
+    counts.filter({ _ == 1 }).size
+  }
+
+  def main(args: Array[String]) = println(solution)
+}
