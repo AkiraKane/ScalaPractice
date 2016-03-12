8135078eb90d1c4619ad3f539abc0a7aa71d4715
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-03-12 03:21:49 +0000

    Problem 143
    Totally not-funny overflow bugs fixed.

--- 140/p143/p143.scala
+++ 140/p143/p143.scala
@@ -1,15 +1,17 @@
 package com.daystrom_data_concepts
 
-import Euler.{gcd, natural}
-import math.{ceil, round, sqrt}
+import Euler.{gcd, natural, naturalBig}
+import math.{ceil, sqrt}
 
 
 object p143 {
   val limit = 120000
 
+  val squares = naturalBig.takeWhile(_ <= limit).map({ i => i*i }).toSet
+
   // http://www.geocities.ws/fredlb37/node9.html
   val primitiveTriples = natural
-    .takeWhile(_ <= ceil(sqrt(limit)))
+    .takeWhile(_ < ceil(sqrt(limit)))
     .flatMap({ m =>
       natural
         .takeWhile(_ < m)
@@ -28,26 +30,15 @@ object p143 {
   val solution = (triples ++ triples.map({ case (p,r,c) => (r,p,c) }))
     .groupBy({ case (p,r,c) => p }).values
     .flatMap({ list =>
-      (list ++ list).combinations(2)
+      list.combinations(2)
         .flatMap({ case Stream((p, r, c), (_, q, b)) =>
-          val a2 = q*q + r*r + q*r
+          val a2 = BigInt(q)*q + BigInt(r)*r + BigInt(q)*r
 
-          if (p + q + r < limit) {
-            lazy val a = round(sqrt(a2)).toInt
-            lazy val angle1 = (a*a + b*b - c*c)/(2.0*a*b)
-            lazy val angle2 = (a*a + c*c - b*b)/(2.0*a*c)
-            lazy val angle3 = (c*c + b*b - a*a)/(2.0*c*b)
-
-            if (a*a == a2 && angle1 > -0.5 && angle2 > -0.5 && angle3 > -0.5)
-              Some(p + q + r)
-            else
-              None
-          }
-          else
-            None
+          if (p + q + r <= limit && squares.contains(a2)) Some(p + q + r)
+          else None
         })
     })
-    .toList.distinct.sum
+    .toSet.sum
 
   def main(args: Array[String]) = println(solution)
 }
