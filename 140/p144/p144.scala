441e0edf6d6e7708049dd8572a66e876061f3d1d
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-03-13 20:53:56 +0000

    Problem 144: Solution

--- 140/p144/p144.scala
+++ 140/p144/p144.scala
@@ -9,9 +9,9 @@ object p143 {
   val firstPoint: PointType = (0.0, 10.1)
   val secondPoint: PointType = (1.4, -9.6)
 
-  def step(p0: PointType, p1: PointType) = {
-    val (x0, y0) = p0
-    val (x1, y1) = p1
+  def step(ps: (PointType, PointType)) = {
+    val ((x0, y0), (x1, y1)) = ps
+
     /*
      * The equation for the normal at p1 comes from [1].  After solving
      * for y using [2], it is written as y = ax + c, where
@@ -24,18 +24,21 @@ object p143 {
      */
     val a = y1 / (4.0 * x1)
     val c = (3.0 * y1) / 4.0
+
     /*
      * The reflection of p0 across the normal comes form [3].
      *
      * 3. https://stackoverflow.com/questions/3306838/algorithm-for-reflecting-a-point-across-a-line
      */
     val d = (x0 + (y0 - c) * a) / (1.0 + a*a)
-    val (x2, y2) = (2.0*d - x0, 2.0*d*a - y0 + 2.0*c)
+    val (x0prime, y0prime) = (2.0*d - x0, 2.0*d*a - y0 + 2.0*c)
+
     /* The line between p1 and p2 is represented as y = mx + b */
-    val m = (y2 - y1) / (x2 - x1)
+    val m = (y0prime - y1) / (x0prime - x1)
     val b = (y1 - m*x1)
-    /* y = mx + b is plugged into 4x^2 + y^2 = 100 */
-    val x = {
+
+    /* y = mx + b is plugged into 4x^2 + y^2 = 100  to find the next point */
+    val x2 = {
       val term1 = 2*math.sqrt(-b*b + 25.0*m*m + 100.0)
       val term2 = -b*m
       val denom = m*m + 4.0
@@ -44,11 +47,14 @@ object p143 {
 
       if (abs(lo - x1) > abs(hi - x1)) lo; else hi
     }
-    val y = m*x + b
-    (x, y)
+    val y2 = m*x2 + b
+
+    ((x1, y1), (x2, y2))
   }
 
-  val solution = step(firstPoint, secondPoint)
+  val solution = Stream.iterate((firstPoint, secondPoint))(step)
+    .takeWhile({ case ((_, _), (x, y)) => abs(x) > 0.01 || y < 0.0 })
+    .length
 
   def main(args: Array[String]) = println(solution)
 }
