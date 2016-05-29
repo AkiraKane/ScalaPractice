5458634e84c829693641640df6427763c4d43150
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-05-29 16:16:06 -0400

    Problem 151: Part II
    This seems correct, but gives incorrect answers.

--- 150/p151/p151.scala
+++ 150/p151/p151.scala
@@ -1,7 +1,12 @@
 package com.daystrom_data_concepts
 
+import scala.collection.mutable
+
 
 object p151 {
+
+  val cache = mutable.Map.empty[(Int, List[String]), Vector[(Int, Int)]]
+  val maxBatch = 15
 
   /**
     * Pull a piece of paper from the envelope and perform the
@@ -24,28 +29,53 @@ object p151 {
     * scenarios that can result from it (where each possibility is
     * equally probable).
     */
-  def scenarios(scenario: Array[String]): Array[Array[String]] = {
-    val result = Array.fill[Array[String]](scenario.length)(scenario)
+  def enumerate(scenario: List[String]): List[List[String]] = {
+    val result = Array.fill[List[String]](scenario.length)(null)
 
     var i = 0; while (i < scenario.length) {
       result(i) = scenario.take(i) ++ pull(scenario(i)) ++ scenario.drop(i+1)
       i += 1
     }
-    result
+
+    result.toList.map(_.toList)
   }
 
-  val all = Iterator.iterate(Array(Array("A1")))({ array =>
-    array.flatMap({ contents =>
-      scenarios(contents).filter(_.length <= 17)
-    })
-  })
-    .take(9)
-    .map({ array =>
-      val n = array.filter(_.length == 1).length.toDouble
-      val d = array.length
-      n / d
-    })
-    .toList
+  /**
+    * Given a batch number (≤ maxBatch) and a scenario, compute a
+    * vector of pairs of integers.  The first integer in each pair is
+    * the number of length-one scenarios which can result, and the second
+    * is the total number of scenarios which can result.
+    */
+  def calculate(batch: Int, scenario: List[String]): Vector[(Int, Int)] = {
+    val key = (batch, scenario.sorted)
 
-  def main(args: Array[String]) = println(all)
+    cache.get(key) match {
+      case Some(vector) => vector
+      case None => {
+        if (batch <= maxBatch) {
+          val scenarios = enumerate(scenario)
+          val lists = scenarios.map({ e => calculate(batch + 1, e) })
+          val num = if (scenario.length == 1) 1; else 0
+          val denom = 1
+          val value = (num, denom) +:
+            (0 until maxBatch - batch)
+            .toVector
+            .map({ i =>
+              val here = lists.map({ list => list(i) })
+              val num = here.map(_._1).sum
+              val denom = here.map(_._2).sum
+              (num, denom)
+            })
+
+          cache.put(key, value)
+          value
+        }
+        else Vector.empty[(Int, Int)]
+      }
+    }
+  }
+
+  val answer = calculate(0, List("A1"))
+
+  def main(args: Array[String]) = println(answer)
 }
